72851c8fe166398f7e34c16864af1008695b8e08
Pirskiy Maxim <pirskiymi98@gmail.com>
2024-02-23 15:14:05 +0300

    start

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './app/index.tsx';
+
+const root = document.getElementById('root');
+
+if (!root) {
+   throw new Error('rood if not defined');
+}
+
+const container = createRoot(root);
+
+container.render(
+   <StrictMode>
+      <App />
+   </StrictMode>,
+);
